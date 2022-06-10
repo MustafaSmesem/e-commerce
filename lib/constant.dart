c31c183cf64f401c18c87b93e777a3852e234cc3
mustafas <mustafa.samism@comodo.com>
2022-06-11 00:30:45 +0300

    theming app

---
+++ lib/constant.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+const primaryColor = Color(0xFF670085);
+
+// light theme colors
+const secondaryLightThemeColor = Color(0xFFF26B7B);
+const textLightThemeColor = Color(0xFFF26B7B);
+const bodyTextLightThemeColor = Color(0xFFF26B7B);
+
+// dark theme colors
+const secondaryDarkThemeColor = Color(0xFFF26B7B);
+const textDarkThemeColor = Color(0xFFF26B7B);
+const bodyTextDarkThemeColor = Color(0xFFF26B7B);
+
+
+
+// background light theme colors
+const lightOrangeColor = Color(0xffffb56b);
+const darkOrangeColor = Color(0xfff39060);
+const pinkColor = Color(0xffac255e);
+
+// background dark theme colors
+
+// const numbers
+const defaultSpacerHeight = 16.0;
+const defaultBorderRadius = 4.0;
+const defaultPadding = 16.0;
---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'constant.dart';
+
+final lightThemeData = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: primaryColor,
+);
+
+final darkThemeData = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: primaryColor,
+);
